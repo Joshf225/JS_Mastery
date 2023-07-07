3d0c54ee153dca1c9be324e1ae493057d606671b
Joshf225 <93196964+Joshf225@users.noreply.github.com>
2023-07-07 15:49:40 +0100

    finished 10 projects

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Projects are made in vanilla JS, HTML and CSS
+
 1. Countdown Timer
 
 - design = https://uidesigndaily.com/posts/ sketch-countdown-timer-day-876
@@ -43,6 +45,31 @@
 - using api(https://developer.themoviedb.org/docs)
 
 7. GitHub Profiles App
+
 - https://uidesigndaily.com/posts/photoshop-profi1e-card-user-day-286
 - search for user
 - get and show data
+
+8. Drawing App
+
+- canvas
+- size
+- color
+
+9. Password Generator
+
+- https://codepen.io/FlorinPop17/full/BaBePej
+- number
+- letters
+- symbols
+- copy to clipboard
+
+10. Weather App
+
+- https://uidesigndaily.com/posts/photoshop-weather-prognosis-day-156
+- enter location
+- get weather
+- have some icons?
+- weather api from: https://openweathermap.org/current , and provided api key from tutorial
+
+free public api's : https://github.com/public-apis/public-apis
---
+++ drawing-app/script.js
@@ -0,0 +1,87 @@
+const canvas = document.getElementById("canvas");
+
+const increaseBtn = document.getElementById("increase");
+const decreaseBtn = document.getElementById("decrease");
+
+const sizeEl = document.getElementById("size");
+const colorEl = document.getElementById("color");
+const clearEl = document.getElementById("clear");
+
+const ctx = canvas.getContext("2d");
+let size = 20;
+let isPressed = false;
+let color = "black";
+let x = undefined;
+let y = undefined;
+
+canvas.addEventListener("mousedown", (e) => {
+  isPressed = true;
+
+  x = e.offsetX;
+  y = e.offsetY;
+});
+
+canvas.addEventListener("mouseup", () => {
+  isPressed = false;
+
+  x = undefined;
+  y = undefined;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (isPressed) {
+    const x2 = e.offsetX;
+    const y2 = e.offsetY;
+
+    drawCircle(x2, y2);
+    drawLine(x, y, x2, y2);
+    x = x2;
+    y = y2;
+  }
+});
+
+function drawCircle(x, y) {
+  ctx.beginPath();
+  ctx.arc(x, y, size, 0, 2 * Math.PI);
+  ctx.fillStyle = color;
+  ctx.fill();
+}
+
+function drawLine(x1, y1, x2, y2) {
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.strokeStyle = color;
+  ctx.lineWidth = size * 2;
+  ctx.stroke();
+}
+
+increaseBtn.addEventListener("click", (e) => {
+  size += 5;
+
+  if (size > 50) {
+    size = 50;
+  }
+  updatSizeOnScreen();
+});
+
+decreaseBtn.addEventListener("click", (e) => {
+  size -= 5;
+
+  if (size < 5) {
+    size = 5;
+  }
+  updatSizeOnScreen();
+});
+
+colorEl.addEventListener("change", (e) => {
+  color = e.target.value;
+});
+
+clearEl.addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+function updatSizeOnScreen() {
+  sizeEl.innerText = size;
+}
