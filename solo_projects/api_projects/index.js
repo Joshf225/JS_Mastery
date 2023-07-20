a4a85a481e507cd733cb947b6d4962e5658aeed0
Joshf225 <93196964+Joshf225@users.noreply.github.com>
2023-07-20 22:14:23 +0100

    worked on some more javascript stuff

--- solo_projects/api_projects/index.js
+++ solo_projects/api_projects/index.js
@@ -10,7 +10,8 @@ const main = document.querySelector("main");
 
 const movie = document.getElementsByClassName("movie");
 
-const clickOverview = document.getElementsByClassName("click-overview");
+const mousePosText = document.getElementById("mouse-pos");
+let mousePos = { x: undefined, y: undefined };
 
 addToSite();
 // async function showAPI() {
@@ -36,6 +37,8 @@ async function logMovies() {
   return respData;
 }
 
+let count = 0;
+
 async function addToSite() {
   const list = await logMovies();
 
@@ -45,7 +48,7 @@ async function addToSite() {
     const exit = document.getElementById("exit");
     const movIMG = IMGPATH + movie.poster_path;
     const movies = document.createElement("div");
-    movies.classList.add("movie");
+    movies.classList.add(`movie`);
     movies.innerHTML = `
         <img
           src="${movIMG}"
@@ -53,43 +56,23 @@ async function addToSite() {
         />
         <div class="movie-info">
           <h3 class="title">${movie.title}</h3>
-          <div class = "click-overview none">
-            <h3 class=""><b>Title:</b> ${movie.title}</h3>
-            <p class="popularity"><b>Popularity:</b> ${movie.popularity}</p>
-            <p class="release_date"><b>Release date:</b> ${movie.release_date}</p>
-            <p class="overview-p"><b>Overview:</b> ${movie.overview}</p>
-          </div>
+        </div>
+        <div class="overview none">
+          <h3 class=""><b>Title:</b> ${movie.title}</h3>
+          <p class="popularity"><b>Popularity:</b> ${movie.popularity}</p>
+          <p class="release_date"><b>Release date:</b> ${movie.release_date}</p>
+          <p class="overview-p"><b>Overview:</b> ${movie.overview}</p> 
         </div>
     `;
     main.appendChild(movies);
+    const clickOverview = document.querySelector(".overview");
+    clickOverview.setAttribute("id", "movie");
+
     movies.addEventListener("click", () => {
-      // alert();
-      main.style.filter = "brightness(0.5)";
-      main.style.transition = "0.4s";
-      clickOverview.classList.toggle("none");
-      // const data = await logMovies();
-      // const movieEl = document.createElement("div");
-      // movieEl.classList.add("overview");
-      // movieEl.innerHTML = `
-      //         <h3 class=""><b>Title:</b> ${data["results"][0].title}</h3>
-      //         <p class="popularity"><b>Popularity:</b> ${data["results"][0].popularity}</p>
-      //         <p class="release_date"><b>Release date:</b> ${data["results"][0].release_date}</p>
-      //         <p class="overview-p"><b>Overview:</b> ${data["results"][0].overview}</p>
-      //   `;
-      // main.appendChild(movieEl);
-      // movies.classList.toggle("none");
-      // exit.addEventListener("click", () => {
-      //   movies.innerHTML = `
-      //   <img
-      //   src="${movIMG}"
-      //   alt="${movie.title}"
-      // />
-      // <div class="movie-info">
-      //   <h3 class="title">${movie.title}</h3>
-      // </div>
-      // `;
-      // });
+      let element = document.getElementById("movie");
+      element.classList.toggle("none");
     });
+    count++;
   });
 }
 
