9e650ba6e78dedc28a0a0414b5dd0c848d6cdc5e
Joshf225 <93196964+Joshf225@users.noreply.github.com>
2023-07-23 20:51:53 +0100

    Completed github profiles project

--- solo_projects/api_projects/github-profiles/index.js
+++ solo_projects/api_projects/github-profiles/index.js
@@ -6,6 +6,8 @@ const main = document.querySelector("main");
 
 const form = document.querySelector("form");
 
+const details = document.getElementById("details");
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const input = document.getElementById("search");
@@ -15,18 +17,41 @@ form.addEventListener("submit", (e) => {
 
 getUser();
 
+// const response = await fetch(APIURL + username + "/repos");
+// const respData = await response.json();
+// let count = 0;
+
 async function getUser(username = "coder") {
   const response = await fetch(APIURL + username);
   const respData = await response.json();
 
   showProfile(respData);
+
+  showRepos(username);
 }
 
-async function searchUser() {
-  const response = await fetch(APIURL + username);
+async function showRepos(username = "coder") {
+  const repos = document.getElementById("repos");
+  const response = await fetch(APIURL + username + "/repos");
   const respData = await response.json();
 
-  showProfile(respData);
+  console.log(respData[20]);
+
+  respData
+    .sort(function (a, b) {
+      return b.stargazers_count - a.stargazers_count;
+    })
+    .slice(0, 10)
+    .forEach((repo) => {
+      const links = document.createElement("a");
+      links.classList.add("repos");
+
+      links.href = `${repo.html_url}`;
+      links.target = `_blank`;
+      links.innerText = `${repo.name}`;
+
+      repos.appendChild(links);
+    });
 }
 
 function showProfile(username) {
@@ -47,13 +72,19 @@ function showProfile(username) {
         <p>
         ${username.bio}
         </p>
-        <div class="details">
+        <div class="details" id="details">
           <p>Followers: ${username.followers}</p>
           <p>Following: ${username.following}</p>
           <p>Repos: ${username.public_repos}</p>
         </div>
+        <div class="repos" id="repos" ></div>
       </div>
 `;
 
+  // repoEl.innerHTML = `
+  //   <a href="${repos.html_url}">${repos.name}</a>
+  // `;
+
   main.appendChild(user);
+  // repos.appendChild(repoEl);
 }
